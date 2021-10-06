e0f32adda0b30b68d8d69e47504bda4d5206ef42
harish29k <90303660+harish29k@users.noreply.github.com>
2021-10-06 11:17:54 +0530

    Add files via upload

---
+++ Welocmetoempwage.java
@@ -0,0 +1,12 @@
+/**
+ *Start with DisplayingWelcome to Employee
+Wage ComputationProgram on Master Branch 
+ *
+ *
+ */
+public class Welocmetoempwage {
+	 public static void main(String[] args){
+			System.out.println("welcome to employ wage computation program");
+			}
+			}
+
